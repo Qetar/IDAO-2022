559ad555c986ee91e10b6603e750efe10b5447e9
ahmed <ahmed42795@gmail.com>
2022-02-15 15:43:13 +0200

    wandb integration

--- baseline.py
+++ baseline.py
@@ -1,26 +1,57 @@
 import yaml
 import json
+import random
 
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import argparse
 
 from pathlib import Path
 from pymatgen.core import Structure
 from sklearn.model_selection import train_test_split
 from megnet.models import MEGNetModel
 from megnet.data.crystal import CrystalGraph
+from wandb.keras import WandbCallback
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.metrics import MeanAbsoluteError
+
+train_datapath = "./data/dichalcogenides_public"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--seed",type=int,default=17)
+parser.add_argument("--epochs",type=int,default=800)
+parser.add_argument("--batch_size",type=int,default=128)
+parser.add_argument("--lr",type=float,default=2e-4)
+parser.add_argument("--cutoff",type=float,default=4.0)
+parser.add_argument("--nblocks",type=int,default=3)
+parser.add_argument("--width",type=int,default=0.8)
+parser.add_argument("--nfeat_bond",type=int,default=10) 
+args = parser.parse_args()
+
+wandb.init(project="IDAO-2022", entity="asu-clowns")
+wandb.config.update(args)
 
 
-
+checkpoint_filepath = "/tmp/checkpoint"
+model_checkpoint_callback = ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=True,
+    monitor="val_ewt",
+    mode="max",
+    save_best_only=True)
 
 def read_pymatgen_dict(file):
     with open(file, "r") as f:
         d = json.load(f)
     return Structure.from_dict(d)
 
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
 
-def energy_within_threshold(prediction, target):
+def ewt(prediction, target):
     # compute absolute error on energy per system.
     # then count the no. of systems where max energy error is < 0.02.
     e_thresh = 0.02
@@ -30,8 +61,8 @@ def energy_within_threshold(prediction, target):
     total = tf.size(target)
     return success / tf.cast(total, tf.int64)
 
-def prepare_dataset(dataset_path):
-    dataset_path = Path(dataset_path)
+def prepare_dataset():
+    dataset_path = Path(train_datapath)
     targets = pd.read_csv(dataset_path / "targets.csv", index_col=0)
     struct = {
         item.name.strip(".json"): read_pymatgen_dict(item)
@@ -41,14 +72,14 @@ def prepare_dataset(dataset_path):
     data = pd.DataFrame(columns=["structures"], index=struct.keys())
     data = data.assign(structures=struct.values(), targets=targets)
 
-    return train_test_split(data, test_size=0.25, random_state=666)
+    return train_test_split(data, test_size=0.25)
 
  
-def prepare_model(cutoff, lr):
-    nfeat_bond = 10
-    r_cutoff = cutoff
+def prepare_model(config):
+    nfeat_bond = config["nfeat_bond"]
+    r_cutoff = config["cutoff"]
     gaussian_centers = np.linspace(0, r_cutoff + 1, nfeat_bond)
-    gaussian_width = 0.8
+    gaussian_width = config["width"]
     
     return MEGNetModel(
         graph_converter=CrystalGraph(cutoff=r_cutoff),
@@ -56,28 +87,26 @@ def prepare_model(cutoff, lr):
         width=gaussian_width,
         loss=["MAE"],
         npass=2,
-        lr=lr,
-        metrics=energy_within_threshold
+        lr=config["lr"],
+        metrics=[ewt,tf.keras.metrics.MeanAbsoluteError()]
     )
-
 
 def main(config):
-    train, test = prepare_dataset(config["datapath"])
-    model = prepare_model(
-        float(config["model"]["cutoff"]),
-        float(config["model"]["lr"]), 
-    )
+    train, test = prepare_dataset()
+    model = prepare_model(config)
     model.train(
         train.structures,
         train.targets,
         validation_structures=test.structures,
         validation_targets=test.targets,
-        epochs=int(config["model"]["epochs"]),
-        batch_size=int(config["model"]["batch_size"]),
+        epochs=config["epochs"],
+        batch_size=config["batch_size"],
+        callbacks=[model_checkpoint_callback,WandbCallback()],
+        save_checkpoint=False,
     )
 
 
 if __name__ == "__main__":
-    with open("config.yaml") as file:
-        config = yaml.safe_load(file)
-    main(config)
+    with open("config.json", 'w') as f:
+        json.dump(vars(args), f, indent=4)
+    main(wandb.config)
--- generate_submission.py
+++ generate_submission.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 import pandas as pd
 
 from pathlib import Path
@@ -6,14 +7,14 @@ from baseline import prepare_model
 from baseline import read_pymatgen_dict
 
 
+checkpoint_path = "/tmp/checkpoint"
+test_datapath = "./data/dichalcogenides_private"
 
 def main(config):
-    model = prepare_model(
-        float(config["model"]["cutoff"]), float(config["model"]["lr"])
-    )
-    model.load_weights(config['checkpoint_path'])
+    model = prepare_model(config)
+    model.load_weights(checkpoint_path)
 
-    dataset_path = Path(config['test_datapath'])
+    dataset_path = Path(test_datapath)
     struct = {item.name.strip('.json'): read_pymatgen_dict(item) for item in (dataset_path/'structures').iterdir()}
     private_test = pd.DataFrame(columns=['id', 'structures'], index=struct.keys())
     private_test = private_test.assign(structures=struct.values())
@@ -21,6 +22,6 @@ def main(config):
     private_test[['predictions']].to_csv('./submission.csv', index_label='id')
 
 if __name__ == '__main__':
-    with open("config.yaml") as file:
-        config = yaml.safe_load(file)
+    with open("config.json",'r') as f:
+        config = json.loads(f)
     main(config)
